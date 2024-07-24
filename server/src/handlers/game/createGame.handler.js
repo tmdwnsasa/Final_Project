31194ff64ea39e9465c45cd69554c5a3f82a7a10
Quah Sim <quahsim@naver.com>
2024-07-24 17:06:12 +0900

    🎉 add : 매치큐세션 추가

--- server/src/handlers/game/createGame.handler.js
+++ server/src/handlers/game/createGame.handler.js
@@ -7,28 +7,35 @@ import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 import { HANDLER_IDS, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
 
-const createGameHandler = ({ socket, userId, payload }) => {
+const createGameHandler = ({ sockets, userIds, payload }) => {
   try {
     const gameId = uuidv4();
     const gameSession = addGameSession(gameId);
 
-    const user = getUserById(userId);
-    if (!user) {
-      throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다');
-    }
-
-    gameSession.addUser(user);
+    //유저ID 목록으로 사용자 정보를 가져옴
+    const users = userIds.map((userId) => getUserById(userId)); 
+    // 각 유저 정보를 게임 세션에 추가
+    users.forEach((user) => {
+      if (!user) {
+        throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다');
+      }
+      gameSession.addUser(user); // Add each user to the game session
+    });
 
     const createGameResponse = createResponse(
       HANDLER_IDS.CREATE_GAME,
       RESPONSE_SUCCESS_CODE,
       { gameId, message: '게임이 생성되었습니다' },
-      userId,
+      userIds,
     );
 
-    socket.write(createGameResponse);
+    sockets.forEach((socket) => {
+      socket.write(createGameResponse);
+    });
   } catch (err) {
-    handlerError(socket, err);
+    sockets.forEach((socket) => {
+      handlerError(sockets, err);
+    });
   }
 };
 
--- server/src/sessions/matchQueue.session.js
+++ server/src/sessions/matchQueue.session.js
@@ -1,25 +1,31 @@
-import createGameHandler from "../handlers/game/createGame.handler";
-import { matchQueueSession } from "./session";
-import { getUserBySocket } from "./user.session";
+import createGameHandler from '../handlers/game/createGame.handler';
+import { matchQueueSession } from './session';
+import { getUserBySocket } from './user.session';
 
-export const addUserinQueue = (socket,data)=>{
-    const user = getUserBySocket(socket);
-    matchQueueSession.push(user);
+export const addUserinQueue = (socket, data) => {
+  const user = getUserBySocket(socket);
+  matchQueueSession.push(user);
 
-    if(matchQueueSession.length>=4){
-        const players = matchQueueSession.splice(0,4);
+  // 매치큐에 4명 이상일 때, createGame.handler에서 게임 세션을 추가 & 큐에서 remove
+  if (matchQueueSession.length >= 4) {
+    const players = matchQueueSession.splice(0, 4);
 
-        players.forEach((player)=>{
-            createGameHandler({socket:player.socket,userId:player.id,payload:data});
-        })
-    }
-    return user;
+    const userIds = players.map((player) => player.id);
+    const sockets = players.map((player) => player.socket);
+
+    createGameHandler({
+      sockets: sockets, 
+      userIds: userIds, 
+      payload: data, 
+    });
+  }
+  return user;
 };
 
-export const removeUserfromQueue = ()=>{
-    const index = matchQueueSession.findIndex((user)=>user.socket ===socket);
-    if (index !==-1){
-        return matchQueueSession.splice(index,1)[0];
-    }
-    return null;
-};
+// export const removeUserfromQueue = (socket) => {
+//   const index = matchQueueSession.findIndex((user) => user.socket === socket);
+//   if (index !== -1) {
+//     return matchQueueSession.splice(index, 1)[0];
+//   }
+//   return null;
+// };
