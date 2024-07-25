aa63e174b607a37a0565b04125b36623bb6c4434
Quah Sim <quahsim@naver.com>
2024-07-25 16:23:43 +0900

    ✨ update  : merged files_240725

--- server/src/utils/docker/init_game_db/create_game_db.sql
+++ server/src/utils/docker/init_game_db/create_game_db.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS GAME_DB;
 USE GAME_DB;
 
-CREATE TABLE score (
+CREATE TABLE IF NOT EXISTS score (
     player_id INT PRIMARY KEY,
     score INT NOT NULL
 );
 
-CREATE TABLE rating (
+CREATE TABLE IF NOT EXISTS rating (
     player_id INT,
     character_id INT,
     win INT,
@@ -14,13 +14,13 @@ CREATE TABLE rating (
     PRIMARY KEY (player_id, character_id)
 );
 
-CREATE TABLE possession (
+CREATE TABLE IF NOT EXISTS possession (
     possession_id INT AUTO_INCREMENT PRIMARY KEY,
     player_id INT,
     character_id INT
 );
 
-CREATE TABLE character (
+CREATE TABLE IF NOT EXISTS `character` (
     character_id INT AUTO_INCREMENT PRIMARY KEY,
     character_name VARCHAR(255), 
     hp INT NOT NULL,
@@ -31,11 +31,11 @@ CREATE TABLE character (
     price INT NOT NULL
 );
 
-CREATE TABLE character_skills (
+CREATE TABLE IF NOT EXISTS character_skills (
     skill_id INT AUTO_INCREMENT PRIMARY KEY,
     skill_name VARCHAR(255), 
     character_id INT,
     damage INT,
     cool_time INT NOT NULL,
-    FOREIGN KEY (character_id) REFERENCES character(character_id)
+    FOREIGN KEY (character_id) REFERENCES `character`(character_id)
 );
