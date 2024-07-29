194258f57ba2559abdec3065e11a2655af95dbc1
nalyn87 <nalyn0807@gmail.com>
2024-07-29 19:56:59 +0900

    ✨ update : 캐릭터, 스킬 내용 추가 함수 기능 제작
    GAME_DB에 내용 추가하는 'insertGameContent' 스크립트 추가함

--- server/src/db/game/game.queries.js
+++ server/src/db/game/game.queries.js
@@ -7,4 +7,8 @@ export const GAME_SQL_QUERIES = {
     'INSERT INTO match_history (game_session_id, player_id, kill, death, damage) VALUES(?, ?, ?, ?, ?)',
   CREATE_MATCH_LOG:
     'INSERT INTO match_log (game_session_id, red_player1_id, red_player2_id, blue_player1_id , blue_player2_id, winner_team, start_time, end_time) VALUES(?, ?, ?, ?, ?, ?, ?, ?)',
+  CREATE_CHARACTER:
+    'INSERT INTO `character` (character_name, hp, speed, power, defense, critical, price) VALUES (?, ?, ?, ?, ?, ?, ?)',
+  CREATE_CHARACTER_SKILLS:
+    'INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES (?, ?, ?, ?, ?, ?, ?)',
 };
