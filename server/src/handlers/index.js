3f287ccbf5475e82258b1d0d73511f7336c12663
rladmswlr <alswndmswlr@naver.com>
2024-08-02 19:42:25 +0900

    ✨ update 스킬 전달기능

--- server/src/handlers/index.js
+++ server/src/handlers/index.js
@@ -9,6 +9,7 @@ import joinLobbyHandler from './game/joinLobby.handler.js';
 import { returnLobbyHandler } from './game/returnLobby.handler.js';
 import matchMakingHandler from './game/matchMaking.handler.js';
 import updateSkillHandler from './game/updateAttack.handler.js';
+
 import updateChattingHandler from './game/updateChatting.handler.js';
 import updateLocationHandler from './game/updateLocation.handler.js';
 import loginHandler from './user/login.handler.js';
