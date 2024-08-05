ae577f55b61e94b10ea4ac281abc611154eedd34
moonhyunhu <ansgusgn@gmaile.com>
2024-08-05 11:02:52 +0900

    ✨ update: db 트랜잭션 적용 및 3번 재시도 적용,♻️ refactor: 임시 게임종료버튼 삭제 및 gameEnd함수 수정

--- server/src/db/game/game.db.js
+++ server/src/db/game/game.db.js
@@ -2,13 +2,15 @@ import pools from '../database.js';
 import { toCamelCase } from '../../utils/transformCase.js';
 import { GAME_SQL_QUERIES } from './game.queries.js';
 import { formatDate } from '../../utils/dateFormatter.js';
+import { asyncSaveScoreRating, saveMatchHistory } from '../../handlers/game/gameEnd.handler.js';
 
-export const createMatchHistory = async (sessionId, playerId, kill, death, damage) => {
-  await pools.GAME_DB.query(GAME_SQL_QUERIES.CREATE_MATCH_HISTORY, [sessionId, playerId, kill, death, damage]);
+export const createMatchHistory = async (connection, sessionId, playerId, kill, death, damage) => {
+  await connection.query(GAME_SQL_QUERIES.CREATE_MATCH_HISTORY, [sessionId, playerId, kill, death, damage]);
   return { sessionId, playerId, kill, death, damage };
 };
 
 export const createMatchLog = async (
+  connection,
   sessionId,
   redPlayer1Id,
   redPlayer2Id,
@@ -18,7 +20,7 @@ export const createMatchLog = async (
   startTime,
 ) => {
   const endTime = Date.now();
-  await pools.GAME_DB.query(GAME_SQL_QUERIES.CREATE_MATCH_LOG, [
+  await connection.query(GAME_SQL_QUERIES.CREATE_MATCH_LOG, [
     sessionId,
     redPlayer1Id,
     redPlayer2Id,
@@ -28,46 +30,47 @@ export const createMatchLog = async (
     formatDate(new Date(startTime)),
     formatDate(new Date(endTime)),
   ]);
+  console.log(`${sessionId}매치로그 저장 완료`);
   return { sessionId, redPlayer1Id, redPlayer2Id, bluePlayer1Id, bluePlayer2Id, winTeam, endTime };
 };
 
-export const createUserScore = async (playerId, score) => {
-  await pools.GAME_DB.query(GAME_SQL_QUERIES.CREATE_USER_SCORE, [playerId, score]);
+export const createUserScore = async (connection, playerId, score) => {
+  await connection.query(GAME_SQL_QUERIES.CREATE_USER_SCORE, [playerId, score]);
   return { playerId, score };
 };
 
-export const createUserRating = async (playerId, characterId, win, lose) => {
-  await pools.GAME_DB.query(GAME_SQL_QUERIES.CREATE_USER_RATING, [playerId, characterId, win, lose]);
+export const createUserRating = async (connection, playerId, characterId, win, lose) => {
+  await connection.query(GAME_SQL_QUERIES.CREATE_USER_RATING, [playerId, characterId, win, lose]);
   return { playerId, characterId, win, lose };
 };
 
-export const updateUserScore = async (playerId, score) => {
-  await pools.GAME_DB.query(GAME_SQL_QUERIES.UPDATE_USER_SCORE, [score, playerId]);
+export const updateUserScore = async (connection, playerId, score) => {
+  await connection.query(GAME_SQL_QUERIES.UPDATE_USER_SCORE, [score, playerId]);
   return { playerId, score };
 };
 
-export const updateUserRating = async (playerId, characterId, win, lose) => {
-  await pools.GAME_DB.query(GAME_SQL_QUERIES.UPDATE_USER_RATING, [win, lose, playerId, characterId]);
+export const updateUserRating = async (connection, playerId, characterId, win, lose) => {
+  await connection.query(GAME_SQL_QUERIES.UPDATE_USER_RATING, [win, lose, playerId, characterId]);
   return { playerId, characterId, win, lose };
 };
 
-export const findUserScoreTable = async (playerId) => {
-  const [rows] = await pools.GAME_DB.query(GAME_SQL_QUERIES.FIND_USER_SCORE_BY_PLAYER_ID, [playerId]);
+export const findUserScoreTable = async (connection, playerId) => {
+  const [rows] = await connection.query(GAME_SQL_QUERIES.FIND_USER_SCORE_BY_PLAYER_ID, [playerId]);
   return toCamelCase(rows[0]);
 };
 
-export const findUserRatingTable = async (playerId) => {
-  const [rows] = await pools.GAME_DB.query(GAME_SQL_QUERIES.FIND_USER_RATING_BY_PLAYER_ID, [playerId]);
+export const findUserRatingTable = async (connection, playerId) => {
+  const [rows] = await connection.query(GAME_SQL_QUERIES.FIND_USER_RATING_BY_PLAYER_ID, [playerId]);
   return toCamelCase(rows[0]);
 };
 
-export const getUserScore = async (playerId) => {
-  const [rows] = await pools.GAME_DB.query(GAME_SQL_QUERIES.FIND_USER_SCORE_BY_PLAYER_ID, [playerId]);
+export const getUserScore = async (connection, playerId) => {
+  const [rows] = await connection.query(GAME_SQL_QUERIES.FIND_USER_SCORE_BY_PLAYER_ID, [playerId]);
   return toCamelCase(rows[0].score);
 };
 
-export const getUserRating = async (playerId) => {
-  const [rows] = await pools.GAME_DB.query(GAME_SQL_QUERIES.FIND_USER_RATING_BY_PLAYER_ID, [playerId]);
+export const getUserRating = async (connection, playerId) => {
+  const [rows] = await connection.query(GAME_SQL_QUERIES.FIND_USER_RATING_BY_PLAYER_ID, [playerId]);
   return toCamelCase(rows[0]);
 };
 
@@ -104,3 +107,39 @@ export const createPossession = async (player_id, character_id) => {
   await pools.GAME_DB.query(GAME_SQL_QUERIES.CREATE_POSSESSION, [player_id, character_id]);
   return { player_id, character_id };
 };
+
+export async function dbSaveTransaction(
+  winTeam,
+  loseTeam,
+  users,
+  gameSession,
+  winnerTeam,
+  startTime,
+) {
+  const connection = await pools.GAME_DB.getConnection();
+  try {
+    await connection.beginTransaction();
+
+    await asyncSaveScoreRating(connection, winTeam, loseTeam);
+    await saveMatchHistory(connection, users, gameSession.sessionId);
+    await createMatchLog(
+      connection,
+      gameSession.sessionId,
+      winTeam[0].playerId,
+      winTeam[1].playerId,
+      loseTeam[0].playerId,
+      loseTeam[1].playerId,
+      winnerTeam,
+      startTime,
+    );
+
+    await connection.commit();
+    console.log('트랜잭션과정 DB저장 성공');
+  } catch (err) {
+    await connection.rollback();
+    console.error('트랜잭션과정 DB저장 실패:', err);
+    throw err;
+  } finally {
+    connection.release();
+  }
+}
--- server/src/handlers/game/gameEnd.handler.js
+++ server/src/handlers/game/gameEnd.handler.js
@@ -1,8 +1,8 @@
 import {
   createMatchHistory,
-  createMatchLog,
   createUserRating,
   createUserScore,
+  dbSaveTransaction,
   findUserRatingTable,
   findUserScoreTable,
   getUserRating,
@@ -11,87 +11,73 @@ import {
   updateUserScore,
 } from '../../db/game/game.db.js';
 import { getGameSession } from '../../sessions/game.session.js';
-import { matchQueueSession } from '../../sessions/session.js';
 import { getUserBySocket } from '../../sessions/user.session.js';
 import { createGameEndPacket } from '../../utils/notification/game.notification.js';
 import { ErrorCodes } from '../../utils/error/errorCodes.js';
 import CustomError from '../../utils/error/customError.js';
-import { getLobbySession } from '../../sessions/lobby.session.js';
 
 export const gameEndHandler = async ({ socket, userId, data }) => {
   try {
-    // const { sessionId } = data;
-    // const gameSession = getGameSession(sessionId);
-    // if (!gameSession) {
-    //   throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '게임 세션을 찾을 수 없습니다');
-    // }
+    const { sessionId } = data;
+    const gameSession = getGameSession(sessionId);
+    if (!gameSession) {
+      throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '게임 세션을 찾을 수 없습니다');
+    }
 
-    // const user = gameSession.getUser(userId);
-    // if (!user) {
-    //   throw new CustomError(ErrorCodes.USER_NOT_FOUND, `${gameSession}게임에 ${userId}님을 찾을 수 없습니다`);
-    // }
+    const user = gameSession.getUser(userId);
+    if (!user) {
+      throw new CustomError(ErrorCodes.USER_NOT_FOUND, `${gameSession}게임에 ${userId}님을 찾을 수 없습니다`);
+    }
 
-    // const findUser = await getUserBySocket(socket); //유저세션에서 해당 유저 찾기
-    // if (!findUser) {
-    //   console.log(`유저세션에 유저가 존재하지 않습니다`);
-    //   return;
-    // }
+    const findUser = await getUserBySocket(socket); //유저세션에서 해당 유저 찾기
+    if (!findUser) {
+      console.log(`유저세션에 유저가 존재하지 않습니다`);
+      return;
+    }
 
-    const gameSession = {
-      sessionId: 'abcddd1',
-      users: [
-        { playerId: 'aaa', characterId: 1, socket: 'a', kill: 2, death: 0, damage: 112 },
-        { playerId: 'mmm', characterId: 2, socket: 'b', kill: 0, death: 0, damage: 88 },
-        { playerId: 'qqq', characterId: 3, socket: 'c', kill: 0, death: 0, damage: 21 },
-        { playerId: 'xxx', characterId: 4, socket: 'd', kill: 0, death: 0, damage: 0 },
-      ],
-    };
+    // const gameSession = {
+    //   sessionId: 'abcddd123',
+    //   users: [
+    //     { playerId: 'aaa', characterId: 1, socket: 'a', kill: 2, death: 0, damage: 112 },
+    //     { playerId: 'mmm', characterId: 2, socket: 'b', kill: 0, death: 0, damage: 88 },
+    //     { playerId: 'qqq', characterId: 3, socket: 'c', kill: 0, death: 0, damage: 21 },
+    //     { playerId: 'xxx', characterId: 4, socket: 'd', kill: 0, death: 0, damage: 0 },
+    //   ],
+    // };
 
+    const users = gameSession.users;
     //현재 서버에서 관리하는 게임세션에는 redTeam blueTeam이 구분이 안되어있어서 매치큐에서 편성하는 코드 긁어옴
-    const players = gameSession.users.splice(0, 4);
-    const redTeam = players.slice(0, 2);
-    const blueTeam = players.slice(2, 4);
-    const myTeam = redTeam.some((user) => user.socket === 'b');
-    const winnerTeamState = myTeam ? 'Win' : 'Lose';
-    const winnerTeam = myTeam ? 'RedTeam' : 'BlueTeam';
-    const loserTeamState = !myTeam ? 'Win' : 'Lose';
-    const winTeam = myTeam ? redTeam : blueTeam;
-    const loseTeam = !myTeam ? redTeam : blueTeam;
+    // const players = gameSession.users.splice(0, 4);
+    // const redTeam = players.slice(0, 2);
+    // const blueTeam = players.slice(2, 4);
+    // const myTeam = redTeam.some((user) => user.socket === 'b');
+    // const winnerTeamState = myTeam ? 'Win' : 'Lose';
+    // const winnerTeam = myTeam ? 'RedTeam' : 'BlueTeam';
+    // const loserTeamState = !myTeam ? 'Win' : 'Lose';
+    // const winTeam = myTeam ? redTeam : blueTeam;
+    // const loseTeam = !myTeam ? redTeam : blueTeam;
 
-    // console.log('111', winnerTeam);
-    // console.log('222', redTeam);
-    // console.log('333', blueTeam);
+    const me = gameSession.getUser(playerId);
+    const winTeamColor = me.team;
+    const loseTeamColor = me.team === 'red' ? 'blue' : 'red';
+    const winTeam = users.filter((user) => user.team === winTeamColor);
+    const loseTeam = users.filter((user) => user.team === loseTeamColor);
 
-    await asyncSaveScoreRating(winTeam, loseTeam);
-    // let kill = 2;
-    // let death = 0;
-    // let damage = 200;
     let startTime = Date.now();
-    // let playerId = 'a';
 
-    const users = redTeam.concat(blueTeam);
-    await saveMatchHistory(users, gameSession.sessionId);
+    for (let i = 1; i < 4; i++) {
+      try {
+        await dbSaveTransaction(winTeam, loseTeam, users, gameSession, winTeamColor, startTime);
+        break;
+      } catch (err) {
+        console.error(`db저장 실패 ${i}번째 시도 중..,${err.message}`);
+        if (i === 3) {
+          console.log('3번 모두 저장 실패!');
+          //db저장 수작업해야하니 추후에 추가
+        }
+      }
+    }
 
-    await createMatchLog(
-      gameSession.sessionId,
-      redTeam[0].playerId,
-      redTeam[1].playerId,
-      blueTeam[0].playerId,
-      blueTeam[1].playerId,
-      winnerTeam,
-      startTime,
-    );
-    console.log('매치로그가 DB에 저장되었습니다');
-
-    //   대전 결과 패킷 - 통지
-    // string winnerTeam = 1
-    // repeated UserHistory users = 2
-
-    // message UserState {
-    //     string playerId = 1
-    //     uint32 kill = 2
-    //     uint32 death = 3
-    // }
     const allUsers = [
       { playerId: 'abc', kill: 2, death: 0 },
       { playerId: 'def', kill: 0, death: 0 },
@@ -107,26 +93,32 @@ export const gameEndHandler = async ({ socket, userId, data }) => {
       result: loserTeamState,
       users: allUsers,
     };
-    const packet = createGameEndPacket(winPayload);
+
+    const winPacket = createGameEndPacket(winPayload);
+    const losePacket = createGameEndPacket(losePayload);
 
     //패킷 통지
-    socket.write(packet);
+    winTeam.forEach((user) => user.socket.write(winPacket));
+    loseTeam.forEach((user) => user.socket.write(losePacket));
+
+    // //패킷 통지
+    // socket.write(packet);
   } catch (err) {
     console.log('gameEndHandler에서 발생한 오류:', err);
   }
 };
 
 //유저 DB에 저장된 score찾기
-async function winSaveScore(winTeam) {
+async function winSaveScore(connection, winTeam) {
   for (const user of winTeam) {
     try {
-      const findUserScore = await findUserScoreTable(user.playerId);
+      const findUserScore = await findUserScoreTable(connection, user.playerId);
       if (!findUserScore) {
-        await createUserScore(user.playerId, 50);
+        await createUserScore(connection, user.playerId, 50);
         console.log(`${user.playerId}님의 Score가 생성`);
       } else {
-        const score = await getUserScore(user.playerId);
-        await updateUserScore(user.playerId, score + 50);
+        const score = await getUserScore(connection, user.playerId);
+        await updateUserScore(connection, user.playerId, score + 50);
         console.log(`${user.playerId}님의 Score가 갱신`);
       }
     } catch (err) {
@@ -135,20 +127,21 @@ async function winSaveScore(winTeam) {
   }
 }
 
-async function loseSaveScore(loseTeam) {
+async function loseSaveScore(connection, loseTeam) {
   for (const user of loseTeam) {
     try {
-      const findUserScore = await findUserScoreTable(user.playerId);
+      const findUserScore = await findUserScoreTable(connection, user.playerId);
       if (!findUserScore) {
-        await createUserScore(user.playerId, 0);
+        await createUserScore(connection, user.playerId, 0);
         console.log(`${user.playerId}님의 Score가 생성`);
       } else {
-        let score = await getUserScore(user.playerId);
+        let score = await getUserScore(connection, user.playerId);
         if (score - 25 <= 0) {
           score = 0;
-          await updateUserScore(user.playerId, score);
+          await updateUserScore(connection, user.playerId, score);
+          console.log(`${user.playerId}님의 Score가 갱신`);
         } else {
-          await updateUserScore(user.playerId, score - 25);
+          await updateUserScore(connection, user.playerId, score - 25);
 
           console.log(`${user.playerId}님의 Score가 갱신`);
         }
@@ -160,16 +153,16 @@ async function loseSaveScore(loseTeam) {
 }
 
 //유저DB에 rating 찾기
-async function winSaveRating(winTeam) {
+async function winSaveRating(connection, winTeam) {
   for (const user of winTeam) {
     try {
-      const findUserRating = await findUserRatingTable(user.playerId);
+      const findUserRating = await findUserRatingTable(connection, user.playerId);
       if (!findUserRating) {
-        await createUserRating(user.playerId, user.characterId, 1, 0);
+        await createUserRating(connection, user.playerId, user.characterId, 1, 0);
         console.log(`${user.playerId}님의 Rating이 생성`);
       } else {
-        const ratingTable = await getUserRating(user.playerId);
-        await updateUserRating(user.playerId, user.characterId, ++ratingTable.win, ratingTable.lose);
+        const ratingTable = await getUserRating(connection, user.playerId);
+        await updateUserRating(connection, user.playerId, user.characterId, ++ratingTable.win, ratingTable.lose);
         console.log(`${user.playerId}님의 Rating이 갱신`);
       }
     } catch (err) {
@@ -178,16 +171,16 @@ async function winSaveRating(winTeam) {
   }
 }
 
-async function loseSaveRating(loseTeam) {
+async function loseSaveRating(connection, loseTeam) {
   for (const user of loseTeam) {
     try {
-      const findUserRating = await findUserRatingTable(user.playerId);
+      const findUserRating = await findUserRatingTable(connection, user.playerId);
       if (!findUserRating) {
-        await createUserRating(user.playerId, user.characterId, 0, 1);
+        await createUserRating(connection, user.playerId, user.characterId, 0, 1);
         console.log(`${user.playerId}님의 Rating이 생성`);
       } else {
-        const ratingTable = await getUserRating(user.playerId);
-        await updateUserRating(user.playerId, user.characterId, ratingTable.win, ++ratingTable.lose);
+        const ratingTable = await getUserRating(connection, user.playerId);
+        await updateUserRating(connection, user.playerId, user.characterId, ratingTable.win, ++ratingTable.lose);
         console.log(`${user.playerId}님의 Rating이 갱신`);
       }
     } catch (err) {
@@ -197,18 +190,18 @@ async function loseSaveRating(loseTeam) {
 }
 
 //순차적으로 저장
-const asyncSaveScoreRating = async (winTeam, loseTeam) => {
-  await winSaveScore(winTeam);
-  await loseSaveScore(loseTeam);
-  await winSaveRating(winTeam);
-  await loseSaveRating(loseTeam);
+export const asyncSaveScoreRating = async (connection, winTeam, loseTeam) => {
+  await winSaveScore(connection, winTeam);
+  await loseSaveScore(connection, loseTeam);
+  await winSaveRating(connection, winTeam);
+  await loseSaveRating(connection, loseTeam);
   console.log('score, rating DB저장 완료');
 };
 
-async function saveMatchHistory(users, sessionId) {
+export async function saveMatchHistory(connection, users, sessionId) {
   for (const user of users) {
     try {
-      await createMatchHistory(sessionId, user.playerId, user.kill, user.death, user.damage);
+      await createMatchHistory(connection, sessionId, user.playerId, user.kill, user.death, user.damage);
       console.log(`${user.playerId}님의 전적 저장완료`);
     } catch (err) {
       console.error(`매치전적 저장 중 에러 발생:`, err);
