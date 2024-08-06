ed64cc056289cba77bb18f39ed98877d93c520d6
4cozm <17328rm@gmail.com>
2024-08-06 10:56:43 +0900

    refector 캐릭터 스키마 데이터 타입 변경

--- server/src/utils/docker/init_game_db/create_game_db.sql
+++ server/src/utils/docker/init_game_db/create_game_db.sql
@@ -46,8 +46,8 @@ CREATE TABLE IF NOT EXISTS `character` (
     character_id INT AUTO_INCREMENT PRIMARY KEY,
     character_name VARCHAR(255), 
     hp INT NOT NULL,
-    speed INT NOT NULL,
-    power INT NOT NULL,
+    speed FLOAT NOT NULL,
+    power FLOAT NOT NULL,
     defense FLOAT NOT NULL,
     critical FLOAT NOT NULL,
     price INT NOT NULL
@@ -64,4 +64,36 @@ CREATE TABLE IF NOT EXISTS character_skills
     `range` INT NULL,
     `scale` INT NULL,
     FOREIGN KEY (character_id) REFERENCES `character`(character_id)
-)
+)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- 근씨 아저씨
+INSERT INTO `character` (character_name, hp, speed, power, defense, critical, price) VALUES ('근씨 아저씨', 150, 5.0, 10.0, 0.1, 0.05, 5000);
+
+-- 원씨 아줌마
+INSERT INTO `character` (character_name, hp, speed, power, defense, critical, price) VALUES ('원씨 아줌마', 100, 4.0, 12.0, 0.08, 0.1, 5000);
+
+-- 힐씨 아줌마
+INSERT INTO `character` (character_name, hp, speed, power, defense, critical, price) VALUES ('힐씨 아줌마', 80, 5.0, 7.0, 0.09, 0, 5000);
+
+-- 탱씨 아저씨
+INSERT INTO `character` (character_name, hp, speed, power, defense, critical, price) VALUES ('탱씨 아저씨', 200, 4.0, 8.0, 0.15, 0.05, 5000);
+
+-- 근씨 아저씨 스킬들
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('괭이질', 1, 1, 1, 1, NULL, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('대쉬', 3, 1, NULL, 10, 4, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('광폭화', 4, 1, NULL, 30, NULL, NULL);
+
+-- 원씨 아줌마 스킬들
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('씨 뿌리기', 2, 2, 1, 1, NULL, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('불장판', 5, 2, 2, 15, 15, 10);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('궁극기', 6, 2, 0.2, 30, 20, NULL);
+
+-- 힐씨 아줌마 스킬들
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('물 뿌리기', 2, 3, 1, 1, NULL, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('새참', 5, 3, 1, 5, 10, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('부활', 6, 3, NULL, 50, 1, NULL);
+
+-- 탱씨 아저씨 스킬들
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('삽질', 1, 4, 1, 1, 1, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('방패막기', 7, 4, NULL, 5, NULL, NULL);
+INSERT INTO character_skills (skill_name, skill_type, character_id, damage_factor, cool_time, `range`, `scale`) VALUES ('궁극기', 5, 4, 0.2, 30, 1, 30);
