f65868c5652740bcd1ad4a6d3863836ab0d340ce
4cozm <17328rm@gmail.com>
2024-08-16 15:12:20 +0900

    test: CI/CD 테스트

--- server/src/handlers/webHook/webHookHandler.js
+++ server/src/handlers/webHook/webHookHandler.js
@@ -9,7 +9,7 @@ const PM2_PROCESS_NAME = process.env.PM2_PROCESS_NAME;
 
 export const webHook = (req, res) => {
   if (req.body.ref === 'refs/heads/main') {
-    // main 브랜치에 push 이벤트 발생 시
+    // main 브랜치에 push/merge 이벤트 발생 시
     const git = simpleGit(REPO_DIR);
     git.pull(async (err, update) => {
       if (err) {
