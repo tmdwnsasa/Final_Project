b3e345df981175dcc3ab2b7858c8921d27d0e884
4cozm <17328rm@gmail.com>
2024-08-16 13:16:36 +0900

    delete:불필요한 파일 제거

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -15,53 +15,9 @@ export const config = {
     typeLength: header.PACKET_TYPE_LENGTH,
   },
   database: {
-    GAME_DB: {
-      name: env.GAME_DB_NAME,
-      user: env.GAME_DB_USER,
-      password: env.GAME_DB_PASSWORD,
-      host: env.GAME_DB_HOST,
-      port: env.GAME_DB_PORT,
-    },
-    USER_DB: {
-      name: env.USER_DB_NAME,
-      user: env.USER_DB_USER,
-      password: env.USER_DB_PASSWORD,
-      host: env.USER_DB_HOST,
-      port: env.USER_DB_PORT,
-    },
-    ERROR_DB: {
-      name: env.ERROR_DB_NAME,
-      user: env.ERROR_DB_USER,
-      password: env.ERROR_DB_PASSWORD,
-      host: env.ERROR_DB_HOST,
-      port: env.ERROR_DB_PORT,
-    },
     REDIS: {
       host: env.REDIS_HOST,
       port: env.REDIS_PORT,
     },
-    SHARDS: {
-      1: {
-        name: env.SHARD_1_NAME,
-        user: env.SHARD_1_USER,
-        password: env.SHARD_1_PASSWORD,
-        host: env.SHARD_1_HOST,
-        port: env.SHARD_1_PORT,
-      },
-      2: {
-        name: env.SHARD_2_NAME,
-        user: env.SHARD_2_USER,
-        password: env.SHARD_2_PASSWORD,
-        host: env.SHARD_2_HOST,
-        port: env.SHARD_2_PORT,
-      },
-      3: {
-        name: env.SHARD_3_NAME,
-        user: env.SHARD_3_USER,
-        password: env.SHARD_3_PASSWORD,
-        host: env.SHARD_3_HOST,
-        port: env.SHARD_3_PORT,
-      }
-    }
   },
 };
--- server/src/constants/env.js
+++ server/src/constants/env.js
@@ -8,41 +8,7 @@ export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
 export const CLIENT_VERSION = process.env.CLIENT_VERSION;
 
-export const USER_DB_NAME = process.env.USER_DB_NAME;
-export const USER_DB_USER = process.env.USER_DB_USER;
-export const USER_DB_PASSWORD = process.env.USER_DB_PASSWORD;
-export const USER_DB_HOST = process.env.USER_DB_HOST;
-export const USER_DB_PORT = process.env.USER_DB_PORT;
-
-export const GAME_DB_NAME = process.env.GAME_DB_NAME;
-export const GAME_DB_USER = process.env.GAME_DB_USER;
-export const GAME_DB_PASSWORD = process.env.GAME_DB_PASSWORD;
-export const GAME_DB_HOST = process.env.GAME_DB_HOST;
-export const GAME_DB_PORT = process.env.GAME_DB_PORT;
-
-export const ERROR_DB_NAME = process.env.ERROR_DB_NAME;
-export const ERROR_DB_USER = process.env.ERROR_DB_USER;
-export const ERROR_DB_PASSWORD = process.env.ERROR_DB_PASSWORD;
-export const ERROR_DB_HOST = process.env.ERROR_DB_HOST;
-export const ERROR_DB_PORT = process.env.ERROR_DB_PORT;
-
 export const REDIS_HOST = process.env.REDIS_HOST;
 export const REDIS_PORT = process.env.REDIS_PORT;
 
-export const SHARD_1_NAME = process.env.AWS_SHARD_1_NAME;
-export const SHARD_1_USER = process.env.AWS_SHARD_1_USER;
-export const SHARD_1_PASSWORD = process.env.AWS_SHARD_1_PASSWORD;
-export const SHARD_1_HOST = process.env.AWS_SHARD_1_HOST;
-export const SHARD_1_PORT = process.env.AWS_SHARD_1_PORT;
-
-export const SHARD_2_NAME = process.env.AWS_SHARD_2_NAME;
-export const SHARD_2_USER = process.env.AWS_SHARD_2_USER;
-export const SHARD_2_PASSWORD = process.env.AWS_SHARD_2_PASSWORD;
-export const SHARD_2_HOST = process.env.AWS_SHARD_2_HOST;
-export const SHARD_2_PORT = process.env.AWS_SHARD_2_PORT;
-
-export const SHARD_3_NAME = process.env.AWS_SHARD_3_NAME;
-export const SHARD_3_USER = process.env.AWS_SHARD_3_USER;
-export const SHARD_3_PASSWORD = process.env.AWS_SHARD_3_PASSWORD;
-export const SHARD_3_HOST = process.env.AWS_SHARD_3_HOST;
-export const SHARD_3_PORT = process.env.AWS_SHARD_3_PORT;
+export const DISCORD_WEB_HOOK = process.dnv.DISCORD_WEB_HOOK;
