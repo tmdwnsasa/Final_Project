9f823b98f8e9490b2cf908103a27809dd65436d6
4cozm <17328rm@gmail.com>
2024-08-18 20:57:33 +0900

    update:custom error 쓰도록 변경

--- server/src/db/apiRequest.js
+++ server/src/db/apiRequest.js
@@ -1,18 +1,17 @@
 import ENDPOINTS from './endPoint.js';
 import { toCamelCase } from '../utils/transformCase.js';
+import CustomError from '../utils/error/customError.js';
 /**
  * DB 서버에 API 요청을 보내는 함수
+ * 스네이크 케이스로 데이터를 전달해 주세요
  * @param {ENDPOINTS} endpoint - ENDPOINTS 에 접근해서 원하는 기능을 선택 (기능까지만 고르세요 url,method X)
  * @param {JSON} data - 보낼 데이터, JSON 형식으로 변경해서 대입해주세요
  * @returns 결과를 toCamelCase로 변환후 반환
  */
-
 const apiRequest = async (endpoint, data) => {
   try {
-    console.log(endpoint.url);
-    console.log(data);
     const response = await fetch(endpoint.url, {
-      method: endpoint.method, // 또는 'GET' 메소드에 따라 변경
+      method: endpoint.method,
       headers: {
         'Content-Type': 'application/json',
       },
@@ -20,7 +19,8 @@ const apiRequest = async (endpoint, data) => {
     });
 
     if (!response.ok) {
-      throw new Error(`DB 서버 API 실패응답 ${response.status}`);
+      const errorResponse = await response.json();
+      throw new CustomError(10021, errorResponse.error);
     }
 
     const result = await response.json();
