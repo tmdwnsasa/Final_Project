48b8c0097c602ecbe9e06e19669f8c0cff479db4
4cozm <17328rm@gmail.com>
2024-08-22 16:49:19 +0900

    fix: 빠진 req,res 부분 추가

--- server/src/handlers/update/updateAnnounce.js
+++ server/src/handlers/update/updateAnnounce.js
@@ -4,12 +4,17 @@ import { getAllUsers } from '../../sessions/user.session.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 const updateRouter = express.Router();
 
-const updateAnnounce = () => {
-  const users = getAllUsers();
-  const packet = "?";
-  users.forEach((user) => {
-    user.socket.write(packet);
-  });
+const updateAnnounce = (req, res) => {
+  try {
+    const users = getAllUsers();
+    const packet = '?';
+    users.forEach((user) => {
+      user.socket.write(packet);
+    });
+    res.status(200).json({ message: 'done' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 updateRouter.post('/', updateAnnounce);
