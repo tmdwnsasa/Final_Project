2f7448dd6c7089bc9b0d23eb4a39111bd4c0c061
diddntjd99 <diddntjd99@gmail.com>
2024-08-23 16:36:31 +0900

    ✨ update : 버전 업데이트

--- server/src/constants/env.js
+++ server/src/constants/env.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const HOST = process.env.HOST;
-export const CLIENT_VERSION = '1.0.0';
+export const CLIENT_VERSION = '1.0.1';
 
 export const REDIS_HOST = process.env.REDIS_HOST;
 export const REDIS_PORT = process.env.REDIS_PORT;
